6b095ec46a57eb72fd7d2bc61e593f19cb572edc
stackrev <farbitherast5888@gmail.com>
2022-05-20 12:54:38 +0430

    make global RedisPubSubModule

--- src/redis/redis-pubsub.module.ts
+++ src/redis/redis-pubsub.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 
 import { RedisPubSubProviders } from './redis-pubsub.providers';
 import { RedisPubSubService } from './redis-pubsub.service';
 
+@Global()
 @Module({
   providers: [...RedisPubSubProviders, RedisPubSubService],
   exports: [...RedisPubSubProviders, RedisPubSubService],
